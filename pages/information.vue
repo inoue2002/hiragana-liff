b3418bd3a82a324c06c178ffda25a8f34a97d0b9
inoue2002 <54356188+youkan2002@users.noreply.github.com>
2021-01-11 20:11:09 +0900

    /information

---
+++ pages/information.vue
@@ -0,0 +1,14 @@
+<template>
+    <v-row justify="center" style='margin-top:10px'>
+        <v-col cols=12>
+            <v-row justify="center">
+                
+            ルビ振りアプリとは？
+            </v-row>
+        </v-col>
+        <v-col cols=10>
+            hogehoge
+
+        </v-col>
+    </v-row>
+</template>
